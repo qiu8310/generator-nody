34030ec67bc488633c2b5a2eccd20f73c8bd3597
Zhonglei Qiu <zhongleiqiu@gmail.com>
2015-03-18 19:32:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,79 @@
+# nody
+
+> Based on [generator-node](https://github.com/yeoman/generator-node-gulp)
+
+This generator creates a new Node.js module, generating all the boilerplate you need to get started with best-of-breed from the gulp ecosystem. The generator also optionally installs additional gulp plugins, see the list below.
+
+
+
+## Installation
+
+Install the generator by running: `npm install -g generator-nody`.
+
+## Options
+
+* `--test-framework=[framework]`
+
+  Defaults to `mocha`. Can be switched to
+  another supported testing framework like `jasmine`.
+
+* `--skip-install`
+
+  Skips the automatic execution of `bower` and `npm` after
+  scaffolding has finished.
+  
+* `--name-case`
+
+  Defaults to `camel`, Can be switched to `kebab`, `snake`
+  
+  
+  
+
+### devDependencies
+
+- Mocha Unit Testing with [gulp-mocha](https://github.com/sindresorhus/gulp-mocha)
+- Automagically lint your code with [gulp-jshint](https://github.com/spenceralger/gulp-jshint)
+- Optional – Check JavaScript code style with [gulp-jscs](https://github.com/sindresorhus/gulp-jscs)
+- Optional – Measuring code coverage with [gulp-istanbul](https://github.com/SBoudrias/gulp-istanbul)
+- Optional – Upload LCOV data to [coveralls.io](http://coveralls.io) with [coveralls](https://github.com/cainus/node-coveralls)
+- Optional – Bump npm versions with [gulp-bump](https://github.com/stevelacy/gulp-bump)
+- Optional - Jasmine Unit Testing with [gulp-jasmine](https://github.com/sindresorhus/gulp-jasmine)
+
+### dependencies
+
+
+- [debug](https://github.com/visionmedia/debug)
+- [Lo-Dash](http://lodash.com/)
+- [q](https://github.com/kriskowal/q)
+
+
+
+## Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```
+yo nody
+```
+
+_Note that this template will generate files in the current directory, so be sure to change to a new directory first if you don't want to overwrite existing files._
+
+
+
+## Support
+
+Should you have any problems or wishes for improvements, feel free to open an [issue](https://github.com/qiu8310/generator-nody/issues).
+
+
+## Articles
+
+Some recommended articles to get you started with node.
+- [Node.js require(s) best practices](http://www.mircozeiss.com/node-js-require-s-best-practices/)
+
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
+
+
---
+++ app/index.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var yeoman = require('yeoman-generator');
+var yoHelper = require('yo-helper');
+var path = require('path');
+
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.testFramework = this.options['test-framework'] || 'mocha';
+    this.nameCase = this.options['name-case'] || 'camel';
+    this.currentYear = new Date().getFullYear();
+    this.currentDate = new Date().toISOString().slice(0,10); // YYY-MM-DD
+  },
+
+  prompting: {
+    welcome: yoHelper.welcome(),
+
+    askForModuleName: yoHelper.askForModuleName(function(data) {
+      this.slugname = this.moduleName = data.moduleName;
+      this.safeSlugname = this.slugname.replace( /-+([a-zA-Z0-9])/g, function (g) { return g[1].toUpperCase(); });
+    }),
+
+    askForGithubUser: yoHelper.askForGithubUser(function(data) {
+      this.githubUser = data;
+    }),
+
+    askForDependencies: yoHelper.askForDependencies(
+      [
+        { name: 'lodash', description: 'A utility library'},
+        { name: 'q', description: 'A library for promises'},
+        { name: 'debug'}
+      ],
+      function(obj, str) {
+        this.usedDependencies = obj;
+        this.dependencies = str;
+      }
+    ),
+
+
+    askForDevDependencies: function () {
+      var cb = this.async();
+
+      var prompts = [{
+        type: 'checkbox',
+        name: 'modules',
+        message: 'Which modules would you like to include?',
+        choices: [{
+          value: 'jscsModule',
+          name: 'jscs (JavaScript Code Style checker)',
+          checked: true
+        }, {
+          value: 'releaseModule',
+          name: 'release (Bump npm versions with Gulp)',
+          checked: true
+        }, {
+          value: 'istanbulModule',
+          name: 'istanbul (JS code coverage tool)',
+          checked: true
+        }]
+      }];
+
+      this.prompt(prompts, function (props) {
+
+        var hasMod = function (mod) {
+          return props.modules.indexOf(mod) !== -1;
+        };
+
+        this.jscsModule = hasMod('jscsModule');
+        this.releaseModule = hasMod('releaseModule');
+        this.istanbulModule = hasMod('istanbulModule');
+        this.coverallsModule = true;
+
+        if (this.istanbulModule) {
+
+          var promptCoveralls = [{
+            type: 'confirm',
+            name: 'coverallsModule',
+            message: 'Would you like add coveralls',
+            default: true
+          }];
+
+          this.prompt(promptCoveralls, function (props) {
+            if (props && props.coverallsModule) {
+              this.coverallsModule = props.coverallsModule;
+            } else {
+              this.coverallsModule = false;
+            }
+            cb();
+
+          }.bind(this));
+
+        } else {
+          cb();
+        }
+
+      }.bind(this));
+    },
+
+
+    askForModuleInfo: function() {
+      var done = this.async();
+
+      var prompts = [{
+        name: 'description',
+        message: 'Description',
+        default: 'The best module ever.'
+      },{
+        name: 'keywords',
+        message: 'Key your keywords (comma to split)'
+      }, {
+        name: 'license',
+        message: 'License',
+        default: 'MIT'
+      }, {
+        type: 'confirm',
+        name: 'cli',
+        message: 'Do you need a CLI?'
+      }, {
+        type: 'confirm',
+        name: 'skipInstall',
+        message: 'Do you need skip npm install?',
+        default: true
+      }];
+
+      this.prompt(prompts, function(props) {
+        props.keywords = props.keywords.split(',').map(function (el) { return el.trim(); });
+        this.props = props;
+        done();
+      }.bind(this));
+    }
+  },
+
+  configuring: function() {
+    var cfg = {srcDir: 'src'};
+    cfg.slugname = this.slugname;
+    this.config.set(cfg);
+  },
+
+  writing: {
+    auto: yoHelper.writing(function(src, target) {
+      if (target.indexOf('slugname') >= 0) {
+        var dir = path.dirname(target), base = path.basename(target);
+
+        base = base.split('.');
+        base[0] = yoHelper.normalize(base[0].replace('slugname', this.slugname), this.nameCase);
+        this.template(src, path.join(dir, base.join('.')));
+
+        return false;
+      }
+    }),
+    custom: function() {
+      if (this.props.cli) {
+        this.template('_ignore/cli._tpl', 'cli.js')
+      }
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.props['skipInstall'] || this.options['skip-install'],
+      bowerInstall: false
+    });
+  }
+});
